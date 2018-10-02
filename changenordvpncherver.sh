9dd2438d3aba395d658446e2b822e2fc269f5701
Luis Puerto <luiss.puerto@gmail.com>
2018-10-02 09:27:45 +0300

    first commit with everything

---
+++ changenordvpncherver.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# This script intent to create a command to easily change the NordVPN Server
+
+cp /etc/openvpn/NordVPN-Servers/ovpn_udp/$1.nordvpn.com.udp.ovpn /etc/openvpn/NordVPN.conf
+sed -i 's_auth-user-pass_auth-user-pass /etc/openvpn/login_g' /etc/openvpn/NordVPN.conf
+systemctl stop openvpn@NordVPN
+systemctl start openvpn@NordVPN
