4d65d3e90db9c7460bedbb967e739eda576f7a19
Luis Puerto <luiss.puerto@gmail.com>
2019-05-05 21:45:24 +0300

    Add a function library and update function on it
    I've change the main script to change the VPN server to the library and add a
    function to the library to update the server list.

--- changenordvpncserverto.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# This script intent to create a command to easily change the NordVPN Server
-
-cp /etc/openvpn/NordVPN-Servers/ovpn_udp/$1.nordvpn.com.udp.ovpn /etc/openvpn/NordVPN.conf
-sed -i 's_auth-user-pass_auth-user-pass /etc/openvpn/login_g' /etc/openvpn/NordVPN.conf
-systemctl restart openvpn@NordVPN
---
+++ nordvpncommands.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+
+# This is a collection of servers to interact with my NordVPN connection on my 
+# raspberry pi
+
+# This function is change the VPN to an specific server
+
+function changenordvpnserverto () {
+	cp /etc/openvpn/NordVPN-Servers/ovpn_udp/$1.nordvpn.com.udp.ovpn /etc/openvpn/NordVPN.conf
+	sed -i 's_auth-user-pass_auth-user-pass /etc/openvpn/login_g' /etc/openvpn/NordVPN.conf
+	systemctl restart openvpn@NordVPN
+}
+
+# This Function is to update the server list
+
+function updatenordvpnserver () {
+	wget -O /etc/openvpn/ovpn.zip https://downloads.nordcdn.com/configs/archives/servers/ovpn.zip
+	unzip -o /etc/openvpn/ovpn.zip -d NordVPN-Servers
+}
